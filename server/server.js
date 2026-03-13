73d48d003600bbd0b4210f897b49d3180b785d14
First Class Admin <admin@example.com>
2026-03-13 16:30:38 +0530

    fix: downgrade express 5 to express 4 for stable Railway deployment

--- server/server.js
+++ server/server.js
@@ -89,7 +89,7 @@ app.use((err, req, res, next) => {
 // Serve Frontend
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
-app.use((req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
 
