7c7ec1abcf5ad14501f1dda4b2fad2d8f984f1e6
First Class Admin <admin@example.com>
2026-03-13 17:04:16 +0530

    fix: upgrade express to 5.2.1 and restore app.use fallback

--- server/server.js
+++ server/server.js
@@ -89,7 +89,7 @@ app.use((err, req, res, next) => {
 // Serve Frontend
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
-app.get("*", (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
 
