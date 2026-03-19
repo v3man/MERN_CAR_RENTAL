935498062a2a4eb9f507aa50bea38633461b0bc8
First Class Admin <admin@example.com>
2026-03-19 20:44:31 +0530

    fix: update catch-all route for new path-to-regexp

--- server/server.js
+++ server/server.js
@@ -91,7 +91,7 @@ app.use((err, req, res, next) => {
 // Serve Frontend Build
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
-app.get("*", (req, res) => {
+app.get("/{*splat}", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
 
