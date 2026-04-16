bb6a734e76ca3610939be09dc38d869a9f95bad9
First Class Admin <admin@example.com>
2026-04-16 11:54:04 +0530

    remove static file serving

--- server/server.js
+++ server/server.js
@@ -93,11 +93,11 @@ app.use((err, req, res, next) => {
 });
 
 // Serve Frontend Build
-app.use(express.static(path.join(__dirname, "../client/dist")));
+// app.use(express.static(path.join(__dirname, "../client/dist")));
 
-app.get("/{*splat}", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
-});
+// app.get("/{*splat}", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+// });
 
 
 
